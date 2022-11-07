07878ae3d3942ae893530094a2a975c8dc1f5fec
davidyoon <qqpo12@naver.com>
2022-11-07 21:53:27 +0900

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by David Yoon on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
