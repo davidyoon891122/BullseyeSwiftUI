51a1bd6a859693b83c9bca22a2e4dcfad496a8ce
David Yoon <qqpo12@naver.com>
2022-12-25 17:35:56 +0900

    Add View Properties and alert Action, state for slider value
    * Added View properties
    * Added alert on the HitMe button
    * Added the state variable sliderValue
    * Applied Type conversion and rounded on sliderValue

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,20 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+
     var body: some View {
         VStack{
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
             Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
             HStack {
                 Text("1")
-                Slider(value: .constant(50), in: 1.0...100.0)
+                    .bold()
+                Slider(value: $sliderValue, in: 1.0...100.0)
                 Text("100")
+                    .bold()
             }
             Button(action: {
-
+                self.alertIsVisible = true
             }) {
                 Text("Hit me")
             }
+            .alert(isPresented: $alertIsVisible,
+                   content: {
+                return Alert(
+                    title: Text("Hello there!"),
+                    message: Text("This slider's value is \(Int(sliderValue.rounded()))."),
+                    dismissButton: .default(Text("Awesome!"))
+                )
+            })
         }
     }
 }
