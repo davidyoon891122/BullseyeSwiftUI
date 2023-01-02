2743d0cbeeca047fc4d682bd5fd8e813437ef15b
David Yoon <qqpo12@naver.com>
2023-01-02 22:27:10 +0900

    Add color set * Added the color set for the theme * Added ZStack for apply background color

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -13,45 +13,62 @@ struct ContentView: View {
     @State private var game = Game()
 
     var body: some View {
-        VStack{
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .kerning(-1.0)
-                .font(.largeTitle)
-                .fontWeight(.black)
-            HStack {
-                Text("1")
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .foregroundColor(Color("TextColor"))
                     .bold()
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
-            }
-            .padding()
-            Button(action: {
-                alertIsVisible = true
-            }) {
-                Text("Hit me".uppercased())
-                    .bold()
-                    .font(.title3)
-            }
-            .padding(20.0)
-            .background(Color.blue)
-            .foregroundColor(Color.white)
-            .cornerRadius(21.0)
-            .alert(isPresented: $alertIsVisible,
-                   content: {
-                let roundedValue = Int(sliderValue.rounded())
-                return Alert(
-                    title: Text("Hello there!"),
-                    message: Text("This slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."),
-                    dismissButton: .default(Text("Awesome!"))
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target))
+                    .foregroundColor(Color("TextColor"))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                HStack {
+                    Text("1")
+                        .foregroundColor(Color("TextColor"))
+                        .bold()
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    Text("100")
+                        .foregroundColor(Color("TextColor"))
+                        .bold()
+                }
+                .padding()
+                Button(action: {
+                    alertIsVisible = true
+                }) {
+                    Text("Hit me".uppercased())
+                        .bold()
+                        .font(.title3)
+                }
+                .padding(20.0)
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    }
                 )
-            })
+                .foregroundColor(Color.white)
+                .cornerRadius(21.0)
+                .alert(isPresented: $alertIsVisible,
+                       content: {
+                    let roundedValue = Int(sliderValue.rounded())
+                    return Alert(
+                        title: Text("Hello there!"),
+                        message: Text("This slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."),
+                        dismissButton: .default(Text("Awesome!"))
+                    )
+                })
+            }
         }
     }
 }
@@ -60,6 +77,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.dark)
             .previewLayout(.fixed(width: 568, height: 320))
     }
 }
