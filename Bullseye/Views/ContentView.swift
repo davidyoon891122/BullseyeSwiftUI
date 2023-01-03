9fe3cbcf9ebce0ac6b395aecd0bf7024922ff545
David Yoon <qqpo12@naver.com>
2023-01-03 21:07:55 +0900

    Separate views
    * Separated the SliderView
    * Separated the HitMeButton
    * Set state values to separated view with Binding values

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -18,45 +18,8 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             VStack{
                 InstructionsView(game: $game)
-                HStack {
-                    Text("1")
-                        .foregroundColor(Color("TextColor"))
-                        .bold()
-                    Slider(value: $sliderValue, in: 1.0...100.0)
-                    Text("100")
-                        .foregroundColor(Color("TextColor"))
-                        .bold()
-                }
-                .padding()
-                Button(action: {
-                    alertIsVisible = true
-                }) {
-                    Text("Hit me".uppercased())
-                        .bold()
-                        .font(.title3)
-                }
-                .padding(20.0)
-                .background(
-                    ZStack {
-                        Color("ButtonColor")
-                        LinearGradient(
-                            gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
-                            startPoint: .top,
-                            endPoint: .bottom
-                        )
-                    }
-                )
-                .foregroundColor(Color.white)
-                .cornerRadius(21.0)
-                .alert(isPresented: $alertIsVisible,
-                       content: {
-                    let roundedValue = Int(sliderValue.rounded())
-                    return Alert(
-                        title: Text("Hello there!"),
-                        message: Text("This slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."),
-                        dismissButton: .default(Text("Awesome!"))
-                    )
-                })
+                SliderView(value: $sliderValue)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
             }
         }
     }
@@ -74,6 +37,59 @@ struct InstructionsView: View {
         }
     }
 }
+
+struct SliderView: View {
+    @Binding var value: Double
+
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: $value, in: 1.0...100.0)
+            SliderLabelText(text: "100")
+        }
+        .padding()
+    }
+}
+
+struct HitMeButton: View {
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+
+    var body: some View {
+        Button(action: {
+            alertIsVisible = true
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+                .font(.title3)
+        }
+        .padding(20.0)
+        .background(
+            ZStack {
+                Color("ButtonColor")
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            }
+        )
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVisible,
+               content: {
+            let roundedValue = Int(sliderValue.rounded())
+            return Alert(
+                title: Text("Hello there!"),
+                message: Text("This slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."),
+                dismissButton: .default(Text("Awesome!"))
+            )
+        })
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
