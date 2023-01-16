6b53312967d9f798a9d4fcc66588cd07333a52e3
David Yoon <qqpo12@naver.com>
2023-01-16 22:44:50 +0900

    Add new Background ringsView
    * Added a ringsView

--- Bullseye/Views/BackgroundView.swift
+++ Bullseye/Views/BackgroundView.swift
@@ -17,8 +17,7 @@ struct BackgroundView: View {
         }
         .padding()
         .background(
-            Color("BackgroundColor")
-                .edgesIgnoringSafeArea(.all)
+            RingsView()
         )
     }
 }
@@ -57,6 +56,21 @@ struct BottomView: View {
             NumberView(title: "SCORE", text: String(game.score))
             Spacer()
             NumberView(title: "ROUND", text: String(game.round))
+        }
+    }
+}
+
+struct RingsView: View {
+    var body: some View {
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            ForEach(1..<6) { ring in
+                let size = CGFloat(ring * 100)
+                Circle()
+                    .stroke(lineWidth: 20.0)
+                    .frame(width: size, height: size)
+            }
         }
     }
 }
